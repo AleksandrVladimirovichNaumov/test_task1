c5354466de206147fd2d66339f128b744bf066f8
alex <skisk8fmx@gmail.com>
2022-07-07 14:24:03 +0300

    test_task

---
+++ restaurant/menu/management/commands/fill_db.py
@@ -0,0 +1,42 @@
+import json
+
+from django.core.management import BaseCommand
+
+from menu.models import FoodCategory, Food
+
+
+class Command(BaseCommand):
+
+    def __init__(self):
+        super().__init__()
+        self.json_path = 'menu/fixtures/'
+
+    def fill_table(self, model, json_file, category=None):
+        """метод для добавления позиций меню из категории"""
+
+        # считываем данные из json
+        with open(self.json_path + json_file, mode='r', encoding='utf-8') as infile:
+            items = json.load(infile)
+
+            # записываем в бд
+        for item in items:
+            new_item = model(**item)
+            # если указана категория, то прописываем ее
+            if category is not None:
+                new_item.category = category
+            new_item.save()
+            print(f'{new_item.name_ru} теперь в бд')
+
+    def handle(self, *args, **options):
+        # обнуляем таблицу с категориями
+        FoodCategory.objects.all().delete()
+        # записываем категории
+        self.fill_table(FoodCategory, 'categories.json')
+        # обнуляем таблицу с едой
+        Food.objects.all().delete()
+        # записываем еду
+        self.fill_table(Food, 'snacks.json', FoodCategory.objects.get(pk=1))
+        self.fill_table(Food, 'soup.json', FoodCategory.objects.get(pk=2))
+        self.fill_table(Food, 'main_dishes.json', FoodCategory.objects.get(pk=3))
+        self.fill_table(Food, 'desserts.json', FoodCategory.objects.get(pk=4))
+        self.fill_table(Food, 'drinks.json', FoodCategory.objects.get(pk=5))
---
+++ restaurant/menu/views.py
@@ -0,0 +1,29 @@
+from django.db.models import Prefetch
+from rest_framework.viewsets import ModelViewSet
+
+from menu.models import FoodCategory, Food
+from menu.serializers import FoodListSerializer
+
+
+class FoodModelViewSet(ModelViewSet):
+    """вьюха для отображения меню по категориям"""
+
+    # используем prefetch_related чтобы исключить пункты меню is_published=False
+    # похожее решение позаимствовал отсюда:
+    # https://stackoverflow.com/questions/28163556/how-do-you-filter-a-nested-serializer-in-django-rest-framework
+    menu = FoodCategory.objects.prefetch_related(
+        Prefetch('food', queryset=Food.objects.filter(is_publish=True), to_attr='filtered_food')).order_by('order_id')
+
+    # делаем список из id категорий, где все пункты меню is_published=False
+    empty_categories = [category.id for category in menu if category.filtered_food == []]
+
+    # удаляем пустые категории из меню
+    queryset = menu.exclude(id__in=empty_categories)
+
+    serializer_class = FoodListSerializer
+
+    def get_serializer_class(self):
+        """метод контроля версий api"""
+        if self.request.version == 'v1':
+            return FoodListSerializer
+
